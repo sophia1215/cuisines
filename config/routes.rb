707bfb998bf16686759b35b8f747a1c2aaf3db22
sophia1215 <code.sophiawang@gmail.com>
2016-05-04 15:11:20 +0800

    generate controller: Users & Recipes & Cuisines & Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  resources :users
+  resources :cuisines, only: [:index, :show]
+  resources :recipes, only: [:show, :edit, :update, :destroy]
+
+  root 'cuisines#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
